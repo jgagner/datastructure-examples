93176729007dc58bda7f09a14d0e7c3272faf6c8
Jerome Gagner <jerome@brocode-inc.com>
2010-04-18 13:43:30 -0700

    Beginnings of a binary search tree

--- java/src/test/java/com/jeromegagner/example/trees/binary/BinaryTreeTest.java
+++ java/src/test/java/com/jeromegagner/example/trees/binary/BinaryTreeTest.java
@@ -1,5 +1,8 @@
 package com.jeromegagner.example.trees.binary;
+import org.junit.Test;
 
+import static org.junit.Assert.*;
+         
 /**
  * Created by IntelliJ IDEA.
  * User: jgagner
@@ -8,4 +11,10 @@ package com.jeromegagner.example.trees.binary;
  * To change this template use File | Settings | File Templates.
  */
 public class BinaryTreeTest {
+
+
+    @Test
+    public void lookupTest(){
+        BinaryTree tree = new BinaryTree();
+    }
 }
